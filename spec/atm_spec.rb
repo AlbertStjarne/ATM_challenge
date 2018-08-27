655c569bc9449518989a494f3bc85d7a36111834
rhon <rhoneil.gabriel@me.com>
2018-08-27 20:24:35 +0200

    created variable account

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,13 @@
 require './lib/atm.rb'
+require 'Date'
 
 describe Atm do
+    let(:account) { instance_double('Account') }
+
+    before do
+        allow(account).to receive(:balance).and_return(100)
+        allow(account).to receive(:balance=)
+    end
     it 'has 1000$ on initialize' do
         expect(subject.funds).to eq 1000
     end
@@ -8,5 +15,10 @@ describe Atm do
     it 'funds are reduced at withdraw' do
         subject.withdraw 50
         expect(subject.funds).to eq 950
+    end
+
+    it 'allow withdraw if account has enough balance.' do
+        expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+        expect(subject.withdraw(45, account)).to eq expected_output
     end
 end
