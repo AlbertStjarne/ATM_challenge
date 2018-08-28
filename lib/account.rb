097495659a0d195e73b9c4153986e3a3c2d7b3be
rhon <rhoneil.gabriel@me.com>
2018-08-28 20:05:19 +0200

    fixed issue with expiry date method

--- lib/account.rb
+++ lib/account.rb
@@ -2,11 +2,11 @@ class Account
 STANDARD_VALIDITY_YRS = 5
 
   def initialize
-    @exp_date = set_expire_date
+    @exp_date = '08/19'
   end
 
 
-  def set_expire_date
+  def exp_date
     Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
 
