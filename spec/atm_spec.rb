328f7fb0a9c0ee2cd6cbe4d1e60609294136faa5
rhon <rhoneil.gabriel@me.com>
2018-08-28 14:48:18 +0200

    issue with account status

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,7 +2,7 @@ require './lib/atm.rb'
 require 'Date'
 
 describe Atm do
-    let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/19') }
+    let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/19', account_status: :active) }
 
     before do
         allow(account).to receive(:balance).and_return(100)
@@ -44,5 +44,10 @@ describe Atm do
         expect(subject.withdraw(6, '1234', account)).to eq expected_output
     end
 
+    it 'reject withdraw if account is disabled' do
+        allow(account).to receive(:account_status).and_return(:active)
+        expected_output = { status: false, message: 'account disabled', date: Date.today}
+        expect(subject.withdraw(100, '1234', account)).to eq expected_output
+    end
 
 end
