e9f00f5b42267f14407a7c813ebeb6b3b59bc7c1
rhon <rhoneil.gabriel@me.com>
2018-08-29 13:30:49 +0200

    set owner to the account

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,13 @@
 class Account
 STANDARD_VALIDITY_YRS = 5
 
-  def initialize
+attr_accessor :owner
+  
+def initialize(attrs = {})
     @exp_date = '08/19'
     @account_status = :active
-
+    @name = set_owner(attrs[:owner])
   end
-
 
   def exp_date
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
@@ -20,6 +21,15 @@ STANDARD_VALIDITY_YRS = 5
     @account_status = :deactivated
   end
 
+  private
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise "An Account owner is required"
+  end
 
 end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,10 @@ require 'Date'
 
 describe Account do
 
+  let(:person) {instance_double('Person', name: 'Santa') }
+
+  subject { described_class.new(owner: person) }
+
   it 'is expected to have an expiry date on initialize' do
     expected_date = Date.today.next_year(5).strftime("%m/%y")
     expect(subject.exp_date).to eq expected_date
@@ -15,6 +19,14 @@ describe Account do
   it 'deactivate account using Instance method' do
     subject.deactivate
     expect(subject.deactivate).to eq :deactivated 
+  end
+
+  it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+  end
+
+  it 'is expected to raise error if no owner is set' do
+    expect { described_class.new }.to raise_error 'An Account owner is required'
   end
 
 end
