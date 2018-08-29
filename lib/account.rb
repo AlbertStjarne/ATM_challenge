7612bff80d5606acc489cf374c29427c4f2cd30a
Albert Stjärne <albert.stjarne@gmail.com>
2018-08-29 10:19:55 +0200

    adding account status

--- lib/account.rb
+++ lib/account.rb
@@ -3,11 +3,21 @@ STANDARD_VALIDITY_YRS = 5
 
   def initialize
     @exp_date = '08/19'
+    @account_status = :active
+
   end
 
 
   def exp_date
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
+  def account_status
+    @account_status = :active
+  end
+
+  def deactivate
+    @account_status = :deactivated
   end
 
 
