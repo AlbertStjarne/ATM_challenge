aa15c19aa4aa517f35cb2b5e242d39dfd4f422d6
rhon <rhoneil.gabriel@me.com>
2018-08-29 09:03:49 +0200

    edit on formula on method exp_date

--- lib/account.rb
+++ lib/account.rb
@@ -7,7 +7,7 @@ STANDARD_VALIDITY_YRS = 5
 
 
   def exp_date
-    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
 
 
