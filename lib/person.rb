ef101cd83baad75eecec27c5df1ac64763122bf7
Albert Stjärne <albert.stjarne@gmail.com>
2018-08-30 10:42:25 +0200

    adding withdraw method and corresponding specs

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,5 @@
 require './lib/account.rb'
+require './lib/atm.rb'
 require 'pry'
 
 class Person
@@ -15,11 +16,34 @@ class Person
   end
 
   def deposit(amount)
-    @account == nil ? missing_account : @account.balance += amount
+    @account == nil ? missing_account : deposit_funds(amount)
+    
   end
 
+  def withdraw(args = {})
+    @account == nil ? missing_account : withdraw_funds(args)
+  end
+  
 
 private
+
+def deposit_funds(amount) 
+  @cash -= amount
+    @account.balance += amount
+end
+
+def withdraw_funds(args)
+  args[:atm] == nil ? missing_atm : atm = args[:atm]
+  account = @account
+  amount = args[:amount]
+  pin = args[:pin]
+  response = atm.withdraw(amount, pin, account)
+  response[:status] == true ? increase_cash(response) : response
+end
+
+def increase_cash(response)
+  @cash += response[:amount]
+end
 
 def set_name(name)
   name == nil ? missing_name : name
@@ -29,8 +53,13 @@ def missing_name
   raise ArgumentError, 'A name is required'
 end
 
- def missing_account
-   raise RuntimeError, 'No account present'
- end
+def missing_account
+  raise RuntimeError, 'No account present'
+end
+
+def missing_atm
+  raise RuntimeError, 'An ATM is required'
+end
+
 
 end
